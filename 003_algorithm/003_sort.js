07c8cac3ac26418f944c9f4366023a7a3b63ad59
version-1 <j.onodera26@gmail.com>
2020-11-03 14:36:31 -0800

    fix test

--- 003_algorithm/003_sort.js
+++ 003_algorithm/003_sort.js
@@ -56,7 +56,7 @@ function insertSort (array) {
  */
 
 function mergeSort (arr) {
-  if (arr.length === 1) {
+  if (arr.length <= 1) {
     // return once we hit an array with a single item
     return arr
   }

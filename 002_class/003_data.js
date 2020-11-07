f265af04f8d564aed4ca8feb3ef2ede0ca77da6b
version-1 <j.onodera26@gmail.com>
2020-11-06 17:39:46 -0800

    add test

--- 002_class/003_data.js
+++ 002_class/003_data.js
@@ -1,6 +1,10 @@
 /**
  *  リストの実装
  *
+ *  オブジェクト指向やJavaScriptの組み込みAPIの成り立ちを理解するためにも
+ *  データ構造をクラスで実装してみましょう。
+ *  実際に使われるリストは要素の型の縛りはありませんが、今回のリストは要素を数値に限定します。
+ *
  */
 
 class List {
@@ -9,17 +13,21 @@ class List {
    * Listクラスのインスタンスを作成する。
    * @constructor
    * @this {List}
-   * @param {Number} array リストのデータ　
+   * @param {Number} array リストのデータ
    */
   constructor(array) {
     this.data = array;
   }
 
+  /** リストの長さを返す
+   *
+   * @return {Number}
+   */
   get size() {
-    return this.data.length
+    return this.data.length;
   }
 
-  /**
+  /** 引数で渡されたデータを取得する
    *
    * @param {Number} index
    * @return {Number}
@@ -28,105 +36,184 @@ class List {
     return this.data[index];
   }
 
+  /** リストの 要素を追加する
+   *
+   * @param {Number} item
+   * @return {Number}
+   */
   push(item) {
     this.data.push(item);
   }
 
+  /** 与えられた引数により、リストの 要素を削除する
+   *
+   * @param {Number} targetIndex
+   * @return {Number}
+   */
   remove(targetIndex) {
+    const num = this.data[targetIndex]
     this.data = this.data.filter((_, index) => index !== targetIndex);
+    return num
   }
 
+  /** リストの 末尾の要素を取得する
+   *
+   * @param {Number} targetIndex
+   * @return {Number}
+   */
   pop() {
-    return this.data.pop()
+    return this.data.pop();
   }
 
+  /** リストの 先頭の要素を取得する
+   *
+   * @return {Number}
+   */
   shift() {
-    return this.data.shift()
+    return this.data.shift();
   }
 
+  /** リストの の中から引数に合致する値を取得する
+   *
+   * XXX: find関数は使わずにfor文、while文を活用して実装しましょう
+   *
+   * @param {Number} target
+   * @return {Number | undefined}
+   */
   find(target) {
     for (let i = 0; i < this.data.length; i++) {
-      const item = this.data[i]
+      const item = this.data[i];
       if (item === target) {
-        return item
+        return item;
       }
     }
   }
 
+  /** リストの の中から引数に合致する値のindexを取得する。見つからない場合は-1を返す
+   *
+   * XXX: findIndex関数は使わずにfor文、while文を活用して実装しましょう
+   *
+   * @param {Number} target
+   * @return {Number}
+   */
   findIndex(target) {
     for (let i = 0; i < this.data.length; i++) {
-      const item = this.data[i]
+      const item = this.data[i];
       if (item === target) {
-        return i
+        return i;
       }
     }
+    return -1
   }
 
+  /** リストの の中から要素に合致する数を取り除く
+   *
+   * 実際のデータ(this.data)に変更を加えることなく、新しい配列を返す形で実装しましょう。
+   *
+   * XXX: filter関数は使わずにfor文、while文を活用して実装しましょう
+   *
+   * @param {Number} target
+   * @return {Number[]}
+   */
   filter(target) {
-    const array = []
+    const list = new List([]);
     for (let i = 0; i < this.data.length; i++) {
-      const item = this.data[i]
+      const item = this.data[i];
       if (item !== target) {
-        array.push(item)
+        list.push(item);
       }
     }
-    return array
+    return list;
   }
 }
 
 /**
  *  スタックの実装
  *
+ *  https://qiita.com/drken/items/6a95b57d2e374a3d3292
+ *
  */
 
 class Stack {
   data = [];
-  constructor(array) {
-    this.data = array;
-  }
 
+  /** スタックの長さを返す
+   *
+   * @return {Number}
+   */
   get size() {
-    return this.data.length
+    return this.data.length;
   }
 
+  /** スタックに要素を追加する
+   *
+   * @param {Number} item
+   * @return {Number}
+   */
   push(item) {
-    this.data.push(item)
+    this.data.push(item);
   }
 
+  /** スタックから要素を取得する
+   *
+   * @param {Number} item
+   * @return {Number}
+   */
   pop() {
-    return this.data.pop()
+    return this.data.pop();
   }
 
+  /** スタックの末尾の要素を参照する
+   *
+   * @param {Number} item
+   * @return {Number}
+   */
   peek() {
-    return this.data[this.length-1]
+    return this.data[this.length - 1];
   }
 }
 
 /**
  *  キューの実装
  *
+ *  https://qiita.com/drken/items/6a95b57d2e374a3d3292
+ *
  */
 
 class Queue {
   data = [];
-  constructor(array) {
-    this.data = array;
-  }
 
+  /** キューの長さを返す
+   *
+   * @return {Number}
+   */
   get size() {
-    return this.data.length
+    return this.data.length;
   }
 
-  push() {
-    this.data.shift(item)
+  /** キューに要素を追加する
+   *
+   * @param {Number} item
+   * @return {Number}
+   */
+  push(item) {
+    this.data.push(item);
   }
 
+  /** キューから要素を取得する
+   *
+   * @return {Number}
+   */
   pull() {
-    return this.data.pop(item)
+    return this.data.shift();
   }
 
+  /** キューの要素を参照する
+   *
+   * @return {Number}
+   */
   peek() {
-    return this.data[this.size -1]
+    return this.data[0];
   }
 }
 
---
+++ __test__/002_class/003_data.spec.js
@@ -0,0 +1,206 @@
+const { logMock } = require('../index');
+const {
+  List,
+  Stack,
+  Queue
+} = require('../../002_class/003_data');
+
+const origin = console.log;
+afterEach(() => (console.log = origin));
+
+describe('List', () => {
+  let outputs = [];
+  beforeEach(() => {
+    outputs = []
+    console.log = logMock(outputs)
+  });
+  test('#size', () => {
+    let obj = new List([1, 2, 3])
+    expect(obj.size).toEqual(3)
+
+    obj = new List([])
+    expect(obj.size).toEqual(0)
+  })
+
+  test('#index', () => {
+    let obj = new List([1, 2, 3])
+    expect(obj.index(0)).toEqual(1)
+    expect(obj.size).toEqual(3)
+
+    expect(obj.index(10)).toEqual(undefined)
+    expect(obj.size).toEqual(3)
+
+    expect(obj.index(-1)).toEqual(undefined)
+    expect(obj.size).toEqual(3)
+
+    obj = new List([])
+    expect(obj.index(0)).toEqual(undefined)
+    expect(obj.size).toEqual(0)
+  })
+
+  test('#push', () => {
+    let obj = new List([])
+    obj.push(1)
+    obj.push(2)
+    obj.push(3)
+
+    expect(obj.index(2)).toEqual(3)
+    expect(obj.size).toEqual(3)
+  })
+
+  test('#remove', () => {
+    let obj = new List([1, 2, 3])
+    expect(obj.remove(1)).toEqual(2)
+
+    expect(obj.index(0)).toEqual(1)
+    expect(obj.index(1)).toEqual(3)
+    expect(obj.size).toEqual(2)
+
+    expect(obj.remove(3)).toEqual(undefined)
+
+    expect(obj.index(0)).toEqual(1)
+    expect(obj.index(1)).toEqual(3)
+    expect(obj.size).toEqual(2)
+
+    obj = new List([])
+    expect(obj.remove(0)).toEqual(undefined)
+    expect(obj.size).toEqual(0)
+  })
+
+  test('#pop', () => {
+    let obj = new List([1, 2, 3])
+    expect(obj.pop()).toEqual(3)
+    expect(obj.size).toEqual(2)
+    expect(obj.pop()).toEqual(2)
+    expect(obj.size).toEqual(1)
+    expect(obj.pop()).toEqual(1)
+    expect(obj.pop()).toEqual(undefined)
+    expect(obj.size).toEqual(0)
+  })
+
+  test('#shift', () => {
+    let obj = new List([1, 2, 3])
+    expect(obj.shift()).toEqual(1)
+    expect(obj.size).toEqual(2)
+    expect(obj.shift()).toEqual(2)
+    expect(obj.size).toEqual(1)
+    expect(obj.shift()).toEqual(3)
+    expect(obj.shift()).toEqual(undefined)
+    expect(obj.size).toEqual(0)
+  })
+
+  test('#find', () => {
+    let obj = new List([1, 2, 3])
+
+    expect(obj.find(2)).toEqual(2)
+    expect(obj.size).toEqual(3)
+
+    expect(obj.find(5)).toEqual(undefined)
+    expect(obj.size).toEqual(3)
+
+    obj = new List([])
+    expect(obj.find(5)).toEqual(undefined)
+    expect(obj.size).toEqual(0)
+  })
+
+  test('#findIndex', () => {
+    let obj = new List([1, 2, 3])
+
+    expect(obj.findIndex(2)).toEqual(1)
+    expect(obj.size).toEqual(3)
+
+    expect(obj.findIndex(5)).toEqual(-1)
+    expect(obj.size).toEqual(3)
+
+    obj = new List([])
+    expect(obj.findIndex(5)).toEqual(-1)
+    expect(obj.size).toEqual(0)
+  })
+
+  test('#filter', () => {
+    let obj = new List([1, 2, 3, 2, 4])
+    let list = obj.filter(2)
+
+    expect(list.size).toEqual(3)
+    expect(list.index(0)).toEqual(1)
+    expect(list.index(1)).toEqual(3)
+    expect(list.index(2)).toEqual(4)
+
+
+    obj = new List([])
+    list = obj.filter(0)
+    expect(list.size).toEqual(0)
+  })
+});
+
+describe('Stack', () => {
+  test('#size', () => {
+    let stack = new Stack()
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.size).toEqual(3)
+  })
+
+  test('#push', () => {
+    let stack = new Stack()
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.size).toEqual(3)
+  })
+
+  test('#pop', () => {
+    let stack = new Stack()
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.pop()).toEqual(3)
+    expect(stack.size).toEqual(2)
+    expect(stack.pop()).toEqual(2)
+    expect(stack.size).toEqual(1)
+    expect(stack.pop()).toEqual(1)
+    expect(stack.size).toEqual(0)
+    expect(stack.pop()).toEqual(undefined)
+    expect(stack.size).toEqual(0)
+  })
+})
+
+describe('Queue', () => {
+  test('#size', () => {
+    let queue = new Queue()
+
+    queue.push(1)
+    queue.push(2)
+    queue.push(3)
+    expect(queue.size).toEqual(3)
+  })
+
+  test('#push', () => {
+    let queue = new Queue()
+
+    queue.push(1)
+    queue.push(2)
+    queue.push(3)
+    expect(queue.size).toEqual(3)
+  })
+
+  test('#pop', () => {
+    let stack = new Queue()
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    expect(stack.pull()).toEqual(1)
+    expect(stack.size).toEqual(2)
+    expect(stack.pull()).toEqual(2)
+    expect(stack.size).toEqual(1)
+    expect(stack.pull()).toEqual(3)
+    expect(stack.size).toEqual(0)
+    expect(stack.pull()).toEqual(undefined)
+    expect(stack.size).toEqual(0)
+  })
+})
